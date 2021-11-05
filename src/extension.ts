63a5de95174b644d146c9bc6ae341073cc64736f
Amir Hardon <ahardon@gmail.com>
2021-11-05 00:43:00 -0700

    generate detailed report

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let showDetails = false;
 	let details: vscode.WebviewPanel | undefined;
+	let reportHtml: string;
 
 	function shouldShowDetails() {
 		if (lastActiveEditor === undefined) {
@@ -23,6 +24,9 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
 				{} // Webview options. More on these later.
 			);
+			if (reportHtml !== undefined) {
+				details.webview.html = reportHtml;
+			}
 			details.onDidDispose(
 				() => {
 					if (shouldShowDetails()) {
@@ -37,6 +41,57 @@ export function activate(context: vscode.ExtensionContext) {
 		} else if (details !== undefined && !shouldShowDetails()) {
 			details.dispose();
 		}
+	}
+
+	function generateDetailedReport(text: string) {
+		return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Readability</title>
+</head>
+<body>
+  <table>
+  <tr>
+    <td>Readability Consensus</td>
+    <td>${rs.textStandard(text, false)}</td>
+  </tr>
+  <tr>
+    <td>Flesch Reading Ease</td>
+    <td>${rs.fleschReadingEase(text)}</td>
+  </tr>
+  <tr>
+    <td>Flesch-Kincaid Grade</td>
+    <td>${rs.fleschKincaidGrade(text)}</td>
+  </tr>
+  <tr>
+    <td>Fog Scale</td>
+    <td>${rs.gunningFog(text)}</td>
+  </tr>
+  <tr>
+    <td>SMOG Index</td>
+    <td>${rs.smogIndex(text)}</td>
+  </tr>
+  <tr>
+    <td>Automated Readability</td>
+    <td>${rs.automatedReadabilityIndex(text)}</td>
+  </tr>
+  <tr>
+    <td>The Coleman-Liau</td>
+    <td>${rs.colemanLiauIndex(text)}</td>
+  </tr>
+  <tr>
+    <td>Linsear Write</td>
+    <td>${rs.linsearWriteFormula(text)}</td>
+  </tr>
+  <tr>
+    <td>Dale-Chall Readability</td>
+    <td>${rs.daleChallReadabilityScore(text)}</td>
+  </tr>
+  </table>
+</body>
+</html>`;
 	}
 
 	const detailsCommandId = 'readability.showDetails';
@@ -56,8 +111,13 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		readabilityStatusBarItem.text = `Readability: ${rs.textStandard(activeEditor.document.getText(), false)}`;
+		let standard = rs.textStandard(activeEditor.document.getText(), false);
+		readabilityStatusBarItem.text = `Readability: ${standard}`;
 		readabilityStatusBarItem.show();
+		reportHtml = generateDetailedReport(activeEditor.document.getText());
+		if (details !== undefined) {
+			details.webview.html = reportHtml;
+		}
 	}
 
 	evaluateReadability();
