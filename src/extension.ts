5ab43ad3ce7b451485e22088c882107dd1b44b77
Amir Hardon <ahardon@gmail.com>
2021-11-05 00:24:20 -0700

    Manage details visibility state

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,51 @@ import * as rs from 'text-readability';
 
 export function activate(context: vscode.ExtensionContext) {
 	let activeEditor = vscode.window.activeTextEditor;
+	let lastActiveEditor = vscode.window.activeTextEditor;
+
+	let showDetails = false;
+	let details: vscode.WebviewPanel | undefined;
+
+	function shouldShowDetails() {
+		if (lastActiveEditor === undefined) {
+			return false;
+		}
+		return showDetails && lastActiveEditor.document.languageId === 'plaintext';
+	}
+
+	function updateDetailsVisibility() {
+		if (details === undefined  && shouldShowDetails()) {
+			details = vscode.window.createWebviewPanel(
+				'readabilityDetails', // Identifies the type of the webview. Used internally
+				'Readability', // Title of the panel displayed to the user
+				vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+				{} // Webview options. More on these later.
+			);
+			details.onDidDispose(
+				() => {
+					if (shouldShowDetails()) {
+						// If we're disposed while the state says we should be showing it means
+						// the uses explicitly closed the details window, so we update
+						// the wanted state to not show an editor.
+						showDetails = false;
+					}
+					details = undefined;
+				}
+			);
+		} else if (details !== undefined && !shouldShowDetails()) {
+			details.dispose();
+		}
+	}
+
+	const detailsCommandId = 'readability.showDetails';
+	context.subscriptions.push(vscode.commands.registerCommand(detailsCommandId, () => {
+		showDetails = !showDetails;
+		updateDetailsVisibility();
+	}));
 
 	let readabilityStatusBarItem: vscode.StatusBarItem;
 	readabilityStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-
+	readabilityStatusBarItem.command = detailsCommandId;
 	context.subscriptions.push(readabilityStatusBarItem);
 
 	function evaluateReadability() {
@@ -24,8 +65,10 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.onDidChangeActiveTextEditor(editor => {
 		activeEditor = editor;
 		if (editor) {
+			lastActiveEditor = editor;
 			evaluateReadability();
 		}
+		updateDetailsVisibility();
 	}, null, context.subscriptions);
 
 	vscode.workspace.onDidChangeTextDocument(event => {
@@ -33,5 +76,4 @@ export function activate(context: vscode.ExtensionContext) {
 			evaluateReadability();
 		}
 	}, null, context.subscriptions);
-
 }
